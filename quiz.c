0c96ea6de5f76254bdb6188cc2f4810697f4d1ef
Tuğberk Alptürk HARMAN <117458800+TugberkAlp@users.noreply.github.com>
2023-05-11 13:05:09 +0300

    Update quiz.c

--- quiz.c
+++ quiz.c
@@ -9,80 +9,73 @@
 
 struct ogrenci
 {
-  char no[10];
-  char ad_soyad[50];
-  double ortalama;
-  int akts_toplam;
-  int giris_yili;
+  char no[50];
+  char ad[50];
+  float ortalama;
+  int akts;
+  int girisYili;
 };
-
-struct ogrenci BasariliOgrenciBul(struct ogrenci ogrenci1, struct ogrenci ogrenci2){
-  if(ogrenci1.ortalama > ogrenci2.ortalama){
-    return ogrenci1;
-  }else if(ogrenci2.ortalama > ogrenci1.ortalama){
-    return ogrenci2;
-  }else
-  {
-    if(ogrenci1.akts_toplam > ogrenci2.akts_toplam){
-      return ogrenci1;
-    }else if(ogrenci2.akts_toplam > ogrenci1.akts_toplam){
-      return ogrenci2;
-    }else
-    {
-      if(ogrenci1.giris_yili < ogrenci2.giris_yili){
-        return ogrenci1;
-      }else if(ogrenci2.giris_yili < ogrenci1.giris_yili){
-        return ogrenci2;
-      }else
-      {
-        srand(time(NULL));
-        int zaman = rand()%2;
-        if(zaman == 0){
-          return ogrenci1;
-        }else if(zaman == 1){
-          return ogrenci2;
-        }
-      }
-    }
+struct ogrenci BasariliOgrenciBul(struct ogrenci o1,struct ogrenci o2)
+{
+  srand(time(NULL));
+  if(o1.ortalama > o2.ortalama){
+    return o1;
+  } else if(o2.ortalama > o1.ortalama){
+    return o2;
   }
-
-
+  if(o1.akts > o2.akts){
+    return o1;
+  } else if(o2.akts > o1.akts){
+    return o2;
+  }
+  if(o1.girisYili < o2.girisYili){
+    return o1;
+  } else if(o2.girisYili < o1.girisYili){
+    return o2;
+  }
+  if(rand()%2 == 1){
+    return o1;
+  } else {
+    return o2;
+  }
 }
 
 int main()
 {
-  struct ogrenci ogrenci1;
-  struct ogrenci ogrenci2;
+  struct ogrenci o1,o2,basarili;
+  char satir;
 
-  printf("1. ogrencinin bilgilerini girin:\n");
-  printf("No: ");
-  scanf("%s", ogrenci1.no);
-  printf("Ad Soyad: ");
-  scanf(" %[^\n]", ogrenci1.ad_soyad);
+  printf("Birinci ogrencinin bilgilerini giriniz:\n");
+  printf("Ogrenci numarasi: ");
+  fgets(o1.no,50,stdin);
+  printf("Ogrencinin adi soyadi: ");
+  fgets(o1.ad,50,stdin);
   printf("Ortalama: ");
-  scanf("%lf", &ogrenci1.ortalama);
-  printf("AKTS Toplam: ");
-  scanf("%d", &ogrenci1.akts_toplam);
-  printf("Giris Yili: ");
-  scanf("%d", &ogrenci1.giris_yili);
-  getchar();
+  scanf("%f",&o1.ortalama);
+  printf("AKTS toplami: ");
+  scanf("%d",&o1.akts);
+  printf("Giris yili: ");
+  scanf("%d",&o1.girisYili);
+  printf("\n");
 
-  printf("2. ogrencinin bilgilerini girin:\n");
-  printf("No: ");
-  scanf("%s", ogrenci2.no);
-  printf("Ad Soyad: ");
-  scanf(" %[^\n]", ogrenci2.ad_soyad);
+  printf("Ikinci ogrencinin bilgilerini giriniz:\n");
+  scanf("%c",&satir);
+  printf("Ogrenci numarasi: ");
+  fgets(o2.no,50,stdin);
+  printf("Ogrencinin adi soyadi: ");
+  fgets(o2.ad,50,stdin);
   printf("Ortalama: ");
-  scanf("%lf", &ogrenci2.ortalama);
-  printf("AKTS Toplam: ");
-  scanf("%d", &ogrenci2.akts_toplam);
-  printf("Giris Yili: ");
-  scanf("%d", &ogrenci2.giris_yili);
-  getchar();
+  scanf("%f",&o2.ortalama);
+  printf("AKTS toplami: ");
+  scanf("%d",&o2.akts);
+  printf("Giris yili: ");
+  scanf("%d",&o2.girisYili);
 
-  struct ogrenci basarili_ogrenci = BasariliOgrenciBul(ogrenci1,ogrenci2);
+  basarili = BasariliOgrenciBul(o1,o2);
+  printf("\nDaha basarili ogrenci:\n");
+  printf("Ogrenci numarasi: %s",basarili.no);
+  printf("Ogrenci adi soyadi: %s\n",basarili.ad);
 
-  printf("Basarili ogrenci: %s %s\n",basarili_ogrenci.no,basarili_ogrenci.ad_soyad);
 
   return 0;
 }
